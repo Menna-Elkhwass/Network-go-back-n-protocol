794e2e82078ad89e8199411e24ad72fb06c1a971
Sar00-Ahmed <104655280+Sar00-Ahmed@users.noreply.github.com>
2022-12-02 20:09:24 +0200

    Update between and send data.c

--- between and send data.c
+++ between and send data.c
@@ -19,3 +19,62 @@ static boolean between(seq_nr a, seq_nr b, seq_nr c)
    to_physical_layer(&S);
    start_timer(frame_nr);
  } 
+
+
+void protocol5(void)
+{
+  seq nr next frame to send; /* MAX SEQ > 1; used for outbound stream */
+  seq nr ack expected; /* oldest frame as yet unacknowledged */
+  seq nr frame expected; /* next frame expected on inbound stream */
+  frame r; /* scratch variable */
+  packet buffer[MAX SEQ + 1]; /* buffers for the outbound stream */
+  seq nr nbuffered; /* number of output buffers currently in use */
+  seq nr i; /* used to index into the buffer array */
+  event type event;
+  enable network layer(); /* allow network layer ready events */
+  ack expected = 0; /* next ack expected inbound */
+  next frame to send = 0; /* next frame going out */
+  frame expected = 0; /* number of frame expected inbound */
+  nbuffered = 0; /* initially no packets are buffered */
+  
+  
+  while (true) {
+    wait for event(&event);            //  frame_arrival = 0, cksum_err =1, timeout = 2, network_layer_ready = 3
+    switch(event) {
+      case network layer ready:       // packet ready to be sent from network layer
+                                       // Accept, save, and transmit a new frame.
+      from network layer(&buffer[next frame to send]); /* fetch new packet */
+      nbuffered = nbuffered + 1;        // expand the sender’s window
+      send data(next frame to send, frame expected, buffer);    /* transmit the frame */
+      inc(next frame to send); /* advance sender’s upper window edge */
+      break;
+
+      case frame arrival: /* a data or control frame has arrived */
+        from physical layer(&r); /* get incoming frame from physical layer */
+        if (r.seq == frame expected) {
+          /* Frames are accepted only in order. */
+          to network layer(&r.info); /* pass packet to network layer */
+          inc(frame expected); /* advance lower edge of receiver’s window */
+        }    /* Ack n implies n − 1, n − 2, etc. Check for this. */
+        while (between(ack expected, r.ack, next frame to send)) {/* Handle piggybacked ack. */
+          nbuffered = nbuffered − 1; /* one frame fewer buffered */
+          stop timer(ack expected); /* frame arrived intact; stop timer */
+          inc(ack expected); /* contract sender’s window */
+        }
+      break;
+        
+      case cksum err: break; /* just ignore bad frames */
+        
+      case timeout: /* trouble; retransmit all outstanding frames */
+      next frame to send = ack expected; /* start retransmitting here */
+      for (i = 1; i <= nbuffered; i++) {
+       send data(next frame to send, frame expected, buffer);/* resend frame */
+       inc(next frame to send); /* prepare to send the next one */
+      }
+   }
+      if (nbuffered < MAX SEQ)
+        enable network layer();
+      else
+        disable network layer();
+  }
+}
