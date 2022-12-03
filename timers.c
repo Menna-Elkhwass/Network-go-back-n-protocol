0441fb7ee1aa91d1c37c26bb4e61ee5cd9c55997
Sar00-Ahmed <104655280+Sar00-Ahmed@users.noreply.github.com>
2022-12-03 22:43:32 +0200

    Delete timers.c

--- timers.c
+++
@@ -1,9 +0,0 @@
-int time=0;
-
-void stop_timer(seq_nr k){
- cout<<"Stop timer for the packet:"<<k<<endl;
- time --;}
- 
- void start_timer(seq_nr k){
-  cout<<"Start timer for the packet:"<<k<<endl;
-  time ++;}
